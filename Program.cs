cda52f067d3ae83ac27c41d6c49eeb35326dfd3d
glafver <glafver22@gmail.com>
2025-04-09 20:14:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml;
+using System;
+
+namespace YourNamespace
+{
+    static class Program
+    {
+        static void Main()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../../../files/gentext.xml");
+
+            XmlNode transUnitNode = doc.SelectSingleNode("//trans-unit[@id='42014']");
+            XmlNode targetNode = null;
+
+            if (transUnitNode != null)
+            {
+                targetNode = transUnitNode.SelectSingleNode("target");
+            }
+
+            if (targetNode != null)
+            {
+                string text = targetNode.InnerText;
+
+                using (StreamWriter writer = new StreamWriter("../../../files/result.txt"))
+                {
+                    writer.WriteLine(text);
+                }
+            }
+
+        }
+    }
+}
